586d9d1329eff31f1cf4355b031f0bc90d167c59
HMDuc <hoangduc1882002@gmail.com>
2021-10-01 09:51:14 +0700

    HMD update code3

--- main.js
+++ main.js
@@ -13,3 +13,7 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+var $ = document.querySelector('h2');
+console.log($)
